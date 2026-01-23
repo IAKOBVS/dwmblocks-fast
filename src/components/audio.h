98d0caca008a0dfba89da30ce0ac5779b7ef1b80
James Tirta Halim <tirtajames45@gmail.com>
2026-01-23 17:30:29 +0700

    audio.h: add missing file

---
+++ src/components/audio.h
@@ -0,0 +1,78 @@
+/* SPDX-License-Identifier: ISC */
+/* Copyright 2025-2026 James Tirta Halim <tirtajames45 at gmail dot com>
+ * This file is part of dwmblocks-fast.
+ *
+ * Permission to use, copy, modify, and/or distribute this software
+ * for any purpose with or without fee is hereby granted, provided that
+ * the above copyright notice and this permission notice appear in all
+ * copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+ * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+ * AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+ * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+ * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. */
+
+#ifndef C_AUDIO_H
+#	define C_AUDIO_H 1
+
+#	include "../config.h"
+
+#	ifdef USE_AUDIO
+
+#		include "../utils.h"
+
+#		ifdef USE_ALSA
+#			include "audio-alsa.h"
+#		endif
+
+#		define C_SPEAKER_UNMUTED "🔉"
+#		define C_SPEAKER_MUTED   "🔇"
+#		define C_MIC_UNMUTED     "🎤"
+#		define C_MIC_MUTED       "🚫"
+
+static char *
+c_write_speaker_vol(char *dst, unsigned int dst_len, const char *unused, unsigned int *interval)
+{
+	char *p = dst;
+	if (!c_read_speaker_muted()) {
+		p = xstpcpy_len(p, S_LITERAL(C_SPEAKER_UNMUTED));
+	} else {
+		p = xstpcpy_len(p, S_LITERAL(C_SPEAKER_MUTED));
+	}
+	*p++ = ' ';
+	p = utoa_p((unsigned int)c_read_speaker_vol(), p);
+	p = xstpcpy_len(p, S_LITERAL(UNIT_USAGE));
+	return p;
+	(void)dst_len;
+	(void)unused;
+	(void)interval;
+}
+
+static char *
+c_write_mic_vol(char *dst, unsigned int dst_len, const char *unused, unsigned int *interval)
+{
+	char *p = dst;
+	if (c_read_mic_muted()) {
+		p = xstpcpy_len(p, S_LITERAL(C_MIC_MUTED));
+	} else {
+		int vol = c_read_mic_vol();
+		if (vol == -1)
+			ERR();
+		p = xstpcpy_len(p, S_LITERAL(C_MIC_UNMUTED));
+		*p++ = ' ';
+		p = utoa_p((unsigned int)vol, p);
+		p = xstpcpy_len(p, S_LITERAL(UNIT_USAGE));
+	}
+	return p;
+	(void)dst_len;
+	(void)unused;
+	(void)interval;
+}
+
+#	endif
+
+#endif /* C_AUDIO_H */
