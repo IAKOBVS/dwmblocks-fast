f1e11b874dac5c3cbeeda69ead0e50d26ad55c06
James Tirta Halim <tirtajames45@gmail.com>
2026-08-03 23:14:08 +0700

    fix bugs

--- blocks/temp.c
+++ blocks/temp.c
@@ -28,7 +28,7 @@ b_write_tempfd_internal(char *dst, unsigned int dst_size, int fd)
 {
 	/* Milidegrees = degrees * 1000 */
 	int read_sz = pread(fd, dst, S_LEN("100") + S_LEN("000") + S_LEN("\n"), 0);
-	if (unlikely(read_sz  == -1))
+	if (unlikely(read_sz <= 0))
 		DIE(return NULL);
 	/* Don't read the newline. */
 	if (*(dst + read_sz - 1) == '\n')
